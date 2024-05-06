8fa50c717bbf2b30adfe1051a8468f547f18ec14
Anna-Sophie Zaitsewa <toddot@protonmail.com>
2024-05-06 12:45:26 +0200

    [settings initializer]: initial commit

---
+++ Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp-permissions-SettingsInitializer.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_PACKAGE_NAME := SettingsInitializer
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := platform
+LOCAL_REQUIRED_MODULES := privapp-permissions-SettingsInitializer.xml
+include $(BUILD_PACKAGE)
---
+++ README.md
@@ -0,0 +1,3 @@
+# Setting initializer
+This module is designed to automatically initialize settings on first boot, so there would be no conflicts with settings
+migrations from frameworks base.
